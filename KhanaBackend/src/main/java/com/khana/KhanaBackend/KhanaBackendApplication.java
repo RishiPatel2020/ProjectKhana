ec232410d988af24fe116cc59bab2c7b2a9b6406
RishiPatel0290 <98559879+RishiPatel2020@users.noreply.github.com>
2022-11-14 22:34:30 -0500

    basic files added to get started

---
+++ KhanaBackend/src/main/java/com/khana/KhanaBackend/KhanaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.khana.KhanaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhanaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhanaBackendApplication.class, args);
+	}
+
+}
---
+++ KhanaBackend/src/test/java/com/khana/KhanaBackend/KhanaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khana.KhanaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhanaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
