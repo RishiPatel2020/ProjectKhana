b9e913c544ee7acce7b20fa88a518c72bb46b268
RishiPatel0290 <98559879+RishiPatel2020@users.noreply.github.com>
2022-11-22 22:34:08 -0500

    Routing fixed

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -11,9 +11,9 @@ function App() {
     <>
     <Nav/>
     <Routes>
-      <Route path="/ProjectKhana" element={<Home/>}/>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
+      <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/about" element={<About/>}/>
 
     </Routes> 
 
--- front-end/src/components/NavBar/Nav.js
+++ front-end/src/components/NavBar/Nav.js
@@ -14,13 +14,13 @@ function NavBar() {
       <Navbar collapseOnSelect expand="lg" bg="secondary" variant="light" >
         <Container >
           {/* <Navbar.Brand href='/'><img src={require("../../Resources/logo.png")}  style={{width:"250px"}} ></img></Navbar.Brand> */}
-          <Navbar.Brand><Link to = "/ProjectKhana">  <img src={require("../../Resources/logo.png")}  style={{width:"250px"}} ></img> </Link></Navbar.Brand>
+          <Navbar.Brand><Link to = "/">  <img src={require("../../Resources/logo.png")}  style={{width:"250px"}} ></img> </Link></Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link ><Link to="/about" style={{ textDecoration: 'none', marginRight:"30px"}}><strong style={{fontFamily:"Comfortaa", fontSize:"27px", color:"rgb(247, 193, 68)"}}>About</strong></Link></Nav.Link>    
-              <Nav.Link ><Link to="/ProjectKhana" style={{ textDecoration: 'none',  marginRight:"30px" }}><strong style={{fontFamily:"Comfortaa", fontSize:"27px", color:"rgb(247, 193, 68)"}}>Plans</strong></Link></Nav.Link>
-              <Nav.Link><Link to="/ProjectKhana" style={{ textDecoration: 'none' ,  marginRight:"90px"}}><strong style={{fontFamily:"Comfortaa", fontSize:"27px", color:"rgb(247, 193, 68)"}}>Help</strong></Link></Nav.Link>
+              <Nav.Link ><Link to="/" style={{ textDecoration: 'none',  marginRight:"30px" }}><strong style={{fontFamily:"Comfortaa", fontSize:"27px", color:"rgb(247, 193, 68)"}}>Plans</strong></Link></Nav.Link>
+              <Nav.Link><Link to="/" style={{ textDecoration: 'none' ,  marginRight:"90px"}}><strong style={{fontFamily:"Comfortaa", fontSize:"27px", color:"rgb(247, 193, 68)"}}>Help</strong></Link></Nav.Link>
 
           
               
